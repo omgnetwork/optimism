be8d53b2df160c63f02c4a94e9c635ad54b9ff97
souradeep-das <dsouradeep2@gmail.com>
2021-06-08 22:00:26 +0530

    feat: register custom messenger

--- packages/omgx/message-relayer-fast/hardhat.config.ts
+++ packages/omgx/message-relayer-fast/hardhat.config.ts
@@ -20,7 +20,8 @@ const config: HardhatUserConfig = {
     localhost: {
 			url: "http://localhost:9545",
 			allowUnlimitedContractSize: true,
-			timeout: 1800000,
+      timeout: 1800000,
+      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
 		},
   },
   solidity: '0.7.6',
--- packages/omgx/message-relayer-fast/scripts/deploy_messenger.js
+++ packages/omgx/message-relayer-fast/scripts/deploy_messenger.js
@@ -1,3 +1,4 @@
+const { getContractFactory } = require('@eth-optimism/contracts')
 require('dotenv').config()
 
 async function main() {
@@ -14,10 +15,29 @@ const L1_Messenger_Deployed = await factory__L1_Messenger.attach(L1_Messenger.ad
 
 console.log('Initializing ...')
 // initialize with address_manager
-const tx0 = await L1_Messenger_Deployed.initialize(
+await L1_Messenger_Deployed.initialize(
     process.env.ETH1_ADDRESS_RESOLVER_ADDRESS
 )
-await tx0.wait()
+
+console.log('Custom L1 Messenger Initialized')
+
+const [deployer] = await ethers.getSigners();
+
+const myContract = getContractFactory(
+  'Lib_AddressManager',
+  deployer
+)
+
+const Lib_AddressManager = await myContract.attach(process.env.ETH1_ADDRESS_RESOLVER_ADDRESS)
+
+// this will fail for non deployer account
+console.log('Registering L1 Messenger...')
+await Lib_AddressManager.setAddress(
+  'OVM_L1CustomCrossDomainMessenger',
+  L1_Messenger.address
+)
+
+console.log('Custom L1 Messenger registered in AddressManager')
 }
 main()
 .then(() => process.exit(0))
