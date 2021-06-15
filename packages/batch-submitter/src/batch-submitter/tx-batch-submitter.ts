01c8bd825bbeb274456afb894e80d1448093b4d4
cby3149 <cby3149@outlook.com>
2021-06-14 21:15:02 -0700

    Removed gasLimit

--- packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
+++ packages/batch-submitter/src/batch-submitter/tx-batch-submitter.ts
@@ -152,7 +152,7 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
           const tx = await this.chainContract.appendQueueBatch(99999999, {
             nonce,
             gasPrice,
-            gasLimit: 9999999
+            // gasLimit: 9999999
           })
           this.logger.info('Submitted appendQueueBatch transaction', {
             txHash: tx.hash,
@@ -261,7 +261,7 @@ export class TransactionBatchSubmitter extends BatchSubmitter {
       const tx = await this.chainContract.appendSequencerBatch(batchParams, {
         nonce,
         gasPrice,
-        gasLimit: 9999999
+        // gasLimit: 9999999
       })
       this.logger.info('Submitted appendSequencerBatch transaction', {
         txHash: tx.hash,
