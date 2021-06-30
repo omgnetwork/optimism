4550ae12e0e618e8e8dc7addd78fc2cb235ccf80
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-06-30 16:51:58 -0700

    improve omgx integration tests

--- packages/omgx/contracts/test/shared/utils.ts
+++ packages/omgx/contracts/test/shared/utils.ts
@@ -10,6 +10,9 @@ import {
 
 require('dotenv').config()
 
+//FIX FIX 
+//Need to fix this so that this file gets the right .env variavles from yhe `docker-compose-omgx-services.yml`
+
 process.env.TEST_PRIVATE_KEY_1 = process.env.TEST_PRIVATE_KEY_1 || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 process.env.TEST_PRIVATE_KEY_2 = process.env.TEST_PRIVATE_KEY_2 || '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba'
 process.env.TEST_PRIVATE_KEY_3 = process.env.TEST_PRIVATE_KEY_3 || '0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e'
