fb4e4119a249166df2d9b6c28ec4329b1e1531a0
cby3149 <cby3149@outlook.com>
2021-07-08 11:04:28 -0700

    Add network flag

--- packages/omgx/contracts/deploy/002-ERC20.deploy.ts
+++ packages/omgx/contracts/deploy/002-ERC20.deploy.ts
@@ -34,28 +34,34 @@ const deployFn: DeployFunction = async (hre) => {
     //Mint a new token on L1 and set up the L1 and L2 infrastructure
     // [initialSupply, name, symbol]
     // this is owned by bobl1Wallet
-    L1ERC20 = await Factory__L1ERC20.deploy(
-      initialSupply,
-      token.name,
-      token.symbol,
-    )
-    await L1ERC20.deployTransaction.wait()
+    if ((hre as any).deployConfig.network === 'local' || token.symbol === 'JLKN') {
+      L1ERC20 = await Factory__L1ERC20.deploy(
+        initialSupply,
+        token.name,
+        token.symbol,
+      )
+      await L1ERC20.deployTransaction.wait()
 
-    const L1ERC20DeploymentSubmission: DeploymentSubmission = {
-      ...L1ERC20,
-      receipt: L1ERC20.receipt,
-      address: L1ERC20.address,
-      abi: L1ERC20Json.abi,
-    };
+      const L1ERC20DeploymentSubmission: DeploymentSubmission = {
+        ...L1ERC20,
+        receipt: L1ERC20.receipt,
+        address: L1ERC20.address,
+        abi: L1ERC20Json.abi,
+      };
 
-    await hre.deployments.save(`L1${token.symbol}`, L1ERC20DeploymentSubmission)
-    console.log(`🌕 ${chalk.red(`L1 ${token.name} deployed to`)} ${chalk.green(L1ERC20.address)}`)
+      await hre.deployments.save(`L1${token.symbol}`, L1ERC20DeploymentSubmission)
+      console.log(`🌕 ${chalk.red(`L1 ${token.name} deployed to`)} ${chalk.green(L1ERC20.address)}`)
+    } else {
+      await hre.deployments.save(`L1${token.symbol}`, { abi: L1ERC20Json.abi, address: token.address })
+      console.log(`🌕 ${chalk.red(`L1 ${token.name} saved to`)} ${chalk.green(token.address)}`)
+    }
 
     //Set up things on L2 for this new token
     // [L2StandardBridgeAddress, L1TokenAddress, tokenName, tokenSymbol]
     L2ERC20 = await Factory__L2ERC20.deploy(
       (hre as any).deployConfig.L2StandardBridgeAddress,
-      L1ERC20.address,
+      ((hre as any).deployConfig.network === 'local' || token.symbol === 'JLKN' ) ?
+        L1ERC20.address : token.address,
       token.name,
       token.symbol,
       {gasLimit: 800000, gasPrice: 0}
