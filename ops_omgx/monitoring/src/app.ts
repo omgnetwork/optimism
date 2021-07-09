806814a582236dcf0734ae9d814711600b1aaf6a
Trang Tran <trang@enya.ai>
2021-07-10 02:08:11 +0700

    monitoring: prevent loading unused script

--- ops_omgx/monitoring/src/app.ts
+++ ops_omgx/monitoring/src/app.ts
@@ -1,14 +1,11 @@
 import * as configs from './configs'
 import logger from './logger'
 
-import { setupProvider } from './monitoring'
-import { doDummyTransaction } from './dummy-transaction'
-
 function validateMonitoring () {
   return configs.l1WsUrl !== undefined && configs.l2WsUrl !== undefined &&
           configs.l1PoolAddress !== undefined && configs.l2PoolAddress !== undefined &&
-          configs.l1AddressManager !== undefined && configs.relayerAddress !== undefined &&
-          configs.sequencerAddress !== undefined && configs.monitoringReconnectSecs !== undefined
+          configs.relayerAddress !== undefined && configs.sequencerAddress !== undefined &&
+          configs.monitoringReconnectSecs !== undefined
 }
 
 function validateDummyTransaction () {
@@ -21,25 +18,31 @@ function validateDummyTransaction () {
           configs.addressOvmEth !== undefined
 }
 
-switch (configs.method) {
-  case configs.AppMethod.Monitoring:
-    if (!validateMonitoring()) {
-      logger.error('Env variables for monitoring is missing!')
+const main = async () => {
+  switch (configs.method) {
+    case configs.AppMethod.Monitoring:
+      if (!validateMonitoring()) {
+        logger.error('Env variables for monitoring is missing!')
+        break
+      }
+      logger.info('Start monitoring service!')
+      const { setupProvider } = await import('./monitoring')
+      setupProvider(configs.OMGXNetwork.L1, configs.l1WsUrl)
+      setupProvider(configs.OMGXNetwork.L2, configs.l2WsUrl)
       break
-    }
-    logger.info('Start monitoring service!')
-    setupProvider(configs.OMGXNetwork.L1, configs.l1WsUrl)
-    setupProvider(configs.OMGXNetwork.L2, configs.l2WsUrl)
-    break
-  case configs.AppMethod.DummyTransaction:
-    if (!validateDummyTransaction()) {
-      logger.error('Env variables for dummy transaction is missing!')
+    case configs.AppMethod.DummyTransaction:
+      if (!validateDummyTransaction()) {
+        logger.error('Env variables for dummy transaction is missing!')
+        break
+      }
+      logger.info('Start dummy transaction service!')
+      const { doDummyTransaction } = await import('./dummy-transaction')
+      doDummyTransaction()
       break
-    }
-    logger.info('Start dummy transaction service!')
-    doDummyTransaction()
-    break
-  default:
-    logger.error('App started with invalid method!')
-    break
+    default:
+      logger.error('App started with invalid method!')
+      break
+  }
 }
+
+main().catch(logger.error)
