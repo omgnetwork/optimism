26ffb1efb4ac0e5ba3b8dec55674dbba21c8a02c
Ino Murko <ino.murko.github@protonmail.com>
2021-07-20 12:28:23 +0200

    vault tests

--- packages/omgx/immutability/test/append-state-batch-proposer.hcl
+++
@@ -1,3 +0,0 @@
-path "immutability-eth-plugin/wallets/proposer/accounts/0x3465333ea987f5620F34F87E95a5C2C421759827/ovm/appendStateBatch" {
-    capabilities = ["create"]
-}
--- packages/omgx/immutability/test/index.js
+++ packages/omgx/immutability/test/index.js
@@ -7,7 +7,7 @@ var Web3 = require('web3');
     const amount = '2'; // Willing to send 2 ethers
     const amountToSend = web3.utils.toWei(amount, "ether"); // Convert to wei value
     //use an account from l1_chain PK 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
-    console.log('asdf');
+    console.log('Funding address with ETH');
     console.log(toAddress);
     console.log(await web3.eth.sendTransaction({ from:'0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc', to:toAddress, value:amountToSend }));
   } catch (e) {
