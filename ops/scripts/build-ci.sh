c0fe2a3d040d526c72a319f9b75007923cd1f4a4
Petar Denev <pdenev@gmail.com>
2021-07-29 20:25:52 +0300

    test to merge logic in gh omgx_integration.yml with integration.yml

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -2,6 +2,8 @@
 function build_images() {
     docker-compose build --parallel -- builder l2geth l1_chain
     docker-compose build --parallel -- deployer dtl batch_submitter relayer integration_tests
+    docker-compose build --parallel -- omgx_deployer omgx_message-relayer-fast
+    docker-compose build --parallel -- gas_oracle
 }
 
 function build_dependencies() {
