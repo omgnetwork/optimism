978549921216247c8e93bcd366c49a325a86ef8f
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-07-31 17:36:39 -0700

    remove stale files that are not being used

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -1,13 +1,23 @@
+yarn 
+yarn build
+
 docker-compose build -- builder
 docker-compose build -- omgx_deployer
 docker-compose build -- omgx_message-relayer-fast
 docker-compose build -- gas_oracle
 docker-compose build -- deployer
+
+docker rmi $(docker images -f "dangling=true" -q)
+
 docker-compose build -- l2geth 
 docker-compose build -- l1_chain
 docker-compose build -- batch_submitter
 docker-compose build -- dtl
 docker-compose build -- relayer
 docker-compose build -- integration_tests
+
+docker rmi $(docker images -f "dangling=true" -q)
+
+docker ps
 
 wait
