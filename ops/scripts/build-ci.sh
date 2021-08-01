4a8e1df748f81733de2ccc640a069fe12df975d6
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-07-31 17:21:56 -0700

    general odds and ends

--- ops/scripts/build-ci.sh
+++ ops/scripts/build-ci.sh
@@ -1,5 +1,7 @@
 docker-compose build -- builder
 docker-compose build -- omgx_deployer
+docker-compose build -- omgx_message-relayer-fast
+docker-compose build -- gas_oracle
 docker-compose build -- deployer
 docker-compose build -- l2geth 
 docker-compose build -- l1_chain
@@ -7,7 +9,5 @@ docker-compose build -- batch_submitter
 docker-compose build -- dtl
 docker-compose build -- relayer
 docker-compose build -- integration_tests
-docker-compose build -- omgx_message-relayer-fast
-docker-compose build -- gas_oracle
 
 wait
