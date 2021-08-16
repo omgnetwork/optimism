946e87c37f52f10b6a39683299feb74c420f78a0
Jesus Meza <jemeza@stanford.edu>
2021-08-16 16:12:01 -0500

    minor changes

--- omgx_examples/bobaChef/scripts/deploy.js
+++ omgx_examples/bobaChef/scripts/deploy.js
@@ -2,7 +2,7 @@
 async function main() {
   // We get the contract to deploy
   const BobaMenu = await ethers.getContractFactory("BobaMenu");
-  const bobaMenu = await BobaMenu.deploy("0xd3bc79679b9e70523cc78c7d8683460a3920dd6741c2f899f3fa417129d4e409", 'test', 'test');
+  const bobaMenu = await BobaMenu.deploy("", 'test', 'test');
 
   console.log("BobaMenu deployed to:", bobaMenu.address);
 }
