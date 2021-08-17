77291fe17b98c1966a0240a26d6c150196640918
Jesus Meza <jemeza@stanford.edu>
2021-08-17 16:18:12 -0500

    Adding boba flavor/topping

--- omgx_examples/bobaMenu/scripts/addBoba.js
+++ omgx_examples/bobaMenu/scripts/addBoba.js
@@ -8,28 +8,28 @@ const main = async () => {
     const l2_provider = new ethers.providers.JsonRpcProvider(env.l2_provider);
     const wallet1 = new ethers.Wallet(env.privateKey1, l2_provider);
 
-    const flavor4 = "Wintermelon Milk Tea"
-    const topping4 = "Lychee Jelly";
-    const commonScore4 = 5;
-    const topping_color4 = [234, 235, 216];
-    const flavor_color4 = [186, 149, 114];
-    const flavor_gradient4 = [4, 2, 4];
+    const flavor3 = "Black Milk Tea";
+    const topping3 = "Tapioca Pearls";
+    const commonScore3 = 15;
+    const topping_color3 = [24, 31, 26];
+    const flavor_color3 = [230, 172, 115];
+    const flavor_gradient3 = [3, 2, 3];
 
-    const flavorStruct4 = [ethers.utils.formatBytes32String(flavor4), // flavor
-                    flavor_color4, // color
-                    commonScore4, // common score
-                    flavor_gradient4 // gradient
+    const flavorStruct3 = [ethers.utils.formatBytes32String(flavor3), // flavor
+                    flavor_color3, // color
+                    commonScore3, // common score
+                    flavor_gradient3 // gradient
                     ];
-    const toppingStruct4 = [ethers.utils.formatBytes32String(topping4), // topping
-                            topping_color4, // color
-                            commonScore4 // common score
+    const toppingStruct3 = [ethers.utils.formatBytes32String(topping3), // topping
+                            topping_color3, // color
+                            commonScore3 // common score
                             ];
     BobaMenu = new ethers.Contract('0x0077B114930ceeB059929f720A1E64D5ed1b2146',
                                     BobaMenuJSON.abi,
                                     wallet1
                                     );
-    await BobaMenu.addFlavor(flavorStruct4);
-    await BobaMenu.addTopping(toppingStruct4);
+    await BobaMenu.addFlavor(flavorStruct3);
+    await BobaMenu.addTopping(toppingStruct3);
 
 }
 
