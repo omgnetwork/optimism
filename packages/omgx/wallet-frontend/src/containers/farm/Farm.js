20038f47a8a9dcf1c44a8baa4001366e558766e3
Osny Netto <netto16@gmail.com>
2021-08-25 10:00:09 -0300

    Fix tabs on earn page

--- packages/omgx/wallet-frontend/src/containers/farm/Farm.js
+++ packages/omgx/wallet-frontend/src/containers/farm/Farm.js
@@ -162,6 +162,10 @@ class Farm extends React.Component {
 
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
     const {
       // Pool
@@ -174,7 +178,6 @@ class Farm extends React.Component {
     const { isMobile } = this.props;
 
     const networkLayer = networkService.L1orL2
-    const handleChange = () => {}
 
     return (
       <>
@@ -190,7 +193,7 @@ class Farm extends React.Component {
         </Grid>
         <Box sx={{ my: 3, width: '100%' }}>
           <Box sx={{ mb: 2 }}>
-            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tabs value={value} onChange={this.handleChange} aria-label="basic tabs example">
               <Tab label="L1 Liquidity Pool" />
               <Tab label="L2 Liquidity Pool" />
             </Tabs>
