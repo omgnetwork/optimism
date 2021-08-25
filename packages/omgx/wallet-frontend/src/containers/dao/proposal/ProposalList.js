42b251aa75d4eeae9c560f6448e03b56a545ac66
sahil kashetwar <sahil@enya.ai>
2021-08-26 03:09:16 +0530

    separate component for the proposal

--- packages/omgx/wallet-frontend/src/containers/dao/proposal/ProposalList.js
+++ packages/omgx/wallet-frontend/src/containers/dao/proposal/ProposalList.js
@@ -16,10 +16,11 @@ limitations under the License. */
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import {openModal} from 'actions/uiAction';
+import { openModal } from 'actions/uiAction';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Button from 'components/button/Button';
+import Proposal from 'components/Proposal/Proposal';
 
 
 
@@ -29,16 +30,13 @@ function ProposalList() {
 
     const dispatch = useDispatch();
 
-    const [dropDownBox, setDropDownBox] = useState(false);
-    const [dropDownBoxInit, setDropDownBoxInit] = useState(true);
 
     return <>
-
         <div className={styles.containerAction}>
             <p className={styles.listTitle}> Proposal List </p>
             <Button
                 type="outline"
-                onClick={()=>{
+                onClick={() => {
                     dispatch(openModal('newProposalModal'))
                 }}
                 style={{
@@ -51,71 +49,8 @@ function ProposalList() {
             > Create Proposal </Button>
         </div>
         <div className={styles.listContainer}>
-            {[1].map((p) => {
-                return <div className={styles.proposalCard}
-                    style={{
-                        background: `${!!dropDownBox ? 'linear-gradient(132.17deg, rgba(255, 255, 255, 0.019985) 0.24%, rgba(255, 255, 255, 0.03) 94.26%)' : 'none'}`,
-                        borderRadius: `${!!dropDownBox ? '12px' : ''}`
-                    }}
-                >
-                    <div
-                        onClick={() => {
-                            setDropDownBox(!dropDownBox);
-                            setDropDownBoxInit(false);
-                        }}>
-                        <div className={styles.proposalHeader}>
-                            <div className={styles.title}>
-                                <p>Proposal #{Math.random() * 100}</p>
-                                <p className={styles.muted}>
-                                    signaturesignaturesignature
-                                </p>
-                            </div>
-                            <ExpandMoreIcon />
-                        </div>
-                        <div className={styles.proposalContent}>
-                            <div>For Votes : <span>"for votes"</span> </div>
-                            <div>Against Votes : <span>"for votes"</span> </div>
-                            <div>Abstain Votes : <span>"for votes"</span> </div>
-                            <div>Vote Percentage : "23%" </div>
-                        </div>
-                    </div>
-
-                    <div
-                        className={dropDownBox ?
-                            styles.dropDownContainer : dropDownBoxInit ? styles.dropDownInit : styles.closeDropDown}
-                    >
-                        <Button
-                            type="outline"
-                            style={{
-                                width: '60%',
-                                padding: '15px 10px',
-                                borderRadius: '8px',
-                                alignSelf: 'center'
-                            }}
-
-                        > Cast Vote For</Button>
-                        <Button
-                            type="primary"
-                            style={{
-                                width: '60%',
-                                padding: '15px 10px',
-                                borderRadius: '8px',
-                                alignSelf: 'center'
-                            }}
-
-                        > Cast Vote Against</Button>
-                        <Button
-                            type="outline"
-                            style={{
-                                width: '60%',
-                                padding: '15px 10px',
-                                borderRadius: '8px',
-                                alignSelf: 'center'
-                            }}
-
-                        > Cast Vote Abstain</Button>
-                    </div>
-                </div>
+            {[1,2,3,4,4,5,6,6,7].map((p, index) => {
+                return <React.Fragment key={index}><Proposal /></React.Fragment>
             })}
 
         </div>
