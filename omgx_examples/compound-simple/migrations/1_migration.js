351d11c72ea391d19a4e76b74b39a2eaa031857d
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-09-01 13:57:05 -0700

    add semi-final DAO contracts and Rinkeby deployment

---
+++ omgx_examples/compound-simple/migrations/1_migration.js
@@ -0,0 +1,116 @@
+const { ethers } = require('ethers')
+require('dotenv');
+
+const sleep = (timeout) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, timeout)
+  })
+}
+
+async function  getTimestamp(){
+  const blockNumber = await web3.eth.getBlockNumber();
+  const block = await web3.eth.getBlock(blockNumber);
+  const timestamp = await block.timestamp;
+  return timestamp;
+}
+
+module.exports = async function (deployer) {
+
+  const accounts = await web3.eth.getAccounts();
+
+  const Comp = artifacts.require('Comp')
+  const Timelock = artifacts.require('Timelock')
+  const GovernorBravoDelegate = artifacts.require('GovernorBravoDelegate')
+  const GovernorBravoDelegator = artifacts.require('GovernorBravoDelegator')
+
+  const user = accounts[0]
+
+  console.log('STARTING HERE')
+  console.log(user)
+  
+  // Deploy Comp
+  await deployer.deploy(Comp, user)
+  const comp = await Comp.deployed()
+  console.log('deployed comp')
+
+  // Deploy Timelock
+  await deployer.deploy(Timelock, user, 0)
+  const timelock = await Timelock.deployed()
+  console.log('deployed timelock')
+
+  // Deploy GovernorBravoDelegate
+  await deployer.deploy(GovernorBravoDelegate)
+  const governorBravoDelegate = await GovernorBravoDelegate.deployed()
+  console.log('deployed delegate')
+
+  // Deploy GovernorBravoDelegator
+  await deployer.deploy(
+    GovernorBravoDelegator,
+    timelock.address,
+    comp.address,
+    timelock.address,
+    governorBravoDelegate.address,
+    10, // the duration of the voting period in blocks
+    10, // the duration of the time between when a proposal is proposed and when the voting period starts
+    ethers.utils.parseEther('100000')
+  )
+  const governorBravoDelegator = await GovernorBravoDelegator.deployed()
+  console.log('deployed delegator')
+
+  console.log('Queue setPendingAdmin');
+
+  const GovernorBravo = await GovernorBravoDelegate.at(
+    governorBravoDelegator.address
+  );
+
+  const eta = (await getTimestamp()) + 300; //wait for 300 seconds
+  
+  const setPendingAdminData = ethers.utils.defaultAbiCoder.encode( // the parameters for the setPendingAdmin function
+    ['address'],
+    [governorBravoDelegator.address]
+    );
+
+  await timelock.queueTransaction(
+    timelock.address,
+    0,
+    'setPendingAdmin(address)', // the function to be called
+    setPendingAdminData,
+    eta
+  )
+
+  console.log(`Time transaction was made: ${await getTimestamp()}`)
+  console.log(`Time at which transaction may be executed: ${eta}`)
+  
+  console.log(`Please be patient and wait for 300 seconds...`)
+
+  await sleep(300 * 1000)
+
+  for(let i = 0; i < 30; i++){
+    console.log(`Attempt: ${i + 1}`)
+    console.log(`\tTimestamp: ${await getTimestamp()}`)
+    try{
+      // Execute the transaction that will set the admin of Timelock to the GovernorBravoDelegator contract
+      await timelock.executeTransaction(
+        timelock.address,
+        0,
+        'setPendingAdmin(address)',
+        setPendingAdminData,
+        eta
+      );
+      console.log('\texecuted setPendingAdmin')
+      break;
+    }catch(error){
+      if(i === 29){
+        console.log("\tFailed. Please try again.\n");
+        return;
+      }
+      // console.log(error);
+      console.log("\tTransaction hasn't surpassed time lock\n");
+    }
+    await sleep(15 * 1000);
+  }
+  GovernorBravo._initiate();
+
+}
---
+++ omgx_examples/compound-simple/truffle-config-ovm.js
@@ -0,0 +1,50 @@
+
+require('dotenv').config();
+const env = process.env;
+const mnemonicPhrase = env.mnemonic
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+const pk_0 = env.pk_0
+const pk_1 = env.pk_1
+const pk_2 = env.pk_2
+
+module.exports = {
+  contracts_build_directory: './build-ovm',
+  networks: {
+    local_l2: {
+      provider: function () {
+        return new HDWalletProvider({
+          privateKeys: [ pk_0, pk_1, pk_2 ],
+          providerOrUrl: 'http://127.0.0.1:8545',
+        })
+      },
+      network_id: 31338,
+      host:  '127.0.0.1',
+      gasPrice: 15000000,
+      gas: 50000000,
+    },
+    rinkeby_l2: {
+      provider: function () {
+        return new HDWalletProvider({
+          privateKeys: [ pk_0, pk_1, pk_2 ],
+          providerOrUrl: 'https://rinkeby.boba.network',
+        })
+      },
+      network_id: 28,
+      host: 'https://rinkeby.boba.network',
+      gasPrice: 15000000,
+      gas: 803900000,
+    }
+  },
+  compilers: {
+    solc: {
+      version: 'node_modules/@eth-optimism/solc',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1,
+        },
+      },
+    },
+  },
+}
