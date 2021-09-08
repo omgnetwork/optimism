43d02b53b893b20eda502c67f2cf47731345445a
sahil kashetwar <sahil@enya.ai>
2021-09-08 23:29:21 +0530

    fix for the history page design

--- packages/omgx/wallet-frontend/src/components/transaction/Transaction.js
+++ packages/omgx/wallet-frontend/src/components/transaction/Transaction.js
@@ -15,7 +15,8 @@ limitations under the License. */
 
 import React, { useState } from 'react'
 
-import { Button as ButtonMUI, Grid } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
+import Button from 'components/button/Button'
 import * as styles from './Transaction.module.scss'
 import * as S from './Transaction.styles'
 import { useTheme } from '@emotion/react'
@@ -47,113 +48,138 @@ function Transaction({
     }
 
     return (
-    <S.TableBody
-      style={{ justifyContent: 'center' }}
-    >
-      <S.TableCell sx={{
-        gap: '5px',
-        width: '98% !important',
-        padding: '10px',
-        alignItems: 'flex-start !important',
-      }}>
-        <div
-          className={dropDownBox ?
-            styles.dropDownContainer : dropDownBoxInit ? styles.dropDownInit : styles.closeDropDown}
-        >
-          <Grid className={styles.dropDownContent} container spacing={1}>
-            L1 Hash:&nbsp;
-            <a className={styles.href} href={detail.l1TxLink} target="_blank" rel="noopener noreferrer">
-              {detail.l1Hash}
-            </a>
-          </Grid>
-          <Grid className={styles.dropDownContent} container spacing={1}>
-            L1 Block:&nbsp;{detail.l1BlockNumber}
-          </Grid>
-          <Grid className={styles.dropDownContent} container spacing={1}>
-            Block Hash:&nbsp;{detail.l1BlockHash}
-          </Grid>
-          <Grid className={styles.dropDownContent} container spacing={1}>
-            L1 From:&nbsp;{detail.l1From}
-          </Grid>
-          <Grid className={styles.dropDownContent} container spacing={1}>
-            L1 To:&nbsp;{detail.l1To}
-          </Grid>
-        </div>
-      </S.TableCell>
-    </S.TableBody>)
+      <S.TableBody
+        style={{ justifyContent: 'center' }}
+      >
+        <S.TableCell sx={{
+          gap: '5px',
+          width: '98% !important',
+          padding: '10px',
+          alignItems: 'flex-start !important',
+        }}>
+          <div
+            className={dropDownBox ?
+              styles.dropDownContainer : dropDownBoxInit ? styles.dropDownInit : styles.closeDropDown}
+          >
+            <Grid className={styles.dropDownContent} container spacing={1}>
+              <Typography variant="body2" >
+                L1 Hash:&nbsp;
+                <Typography className={styles.muted} variant="body2" component="span">
+                  <a className={styles.href} href={detail.l1TxLink} target="_blank" rel="noopener noreferrer">
+                    {detail.l1Hash}
+                  </a>
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid className={styles.dropDownContent} container spacing={1}>
+              <Typography variant="body2" >
+                L1 Block:&nbsp;
+                <Typography className={styles.muted} variant="body2" component="span">
+                  {detail.l1BlockNumber}
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid className={styles.dropDownContent} container spacing={1}>
+              <Typography variant="body2" >
+                Block Hash:&nbsp;
+                <Typography className={styles.muted} variant="body2" component="span">
+                  {detail.l1BlockHash}
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid className={styles.dropDownContent} container spacing={1}>
+              <Typography variant="body2" >
+                L1 From:&nbsp;
+                <Typography className={styles.muted} variant="body2" component="span">
+                  {detail.l1From}
+                </Typography>
+              </Typography>
+            </Grid>
+            <Grid className={styles.dropDownContent} container spacing={1}>
+              <Typography variant="body2" >
+                L1 To:&nbsp;
+                <Typography className={styles.muted} variant="body2" component="span">
+                  {detail.l1To}
+                </Typography>
+              </Typography>
+            </Grid>
+          </div>
+        </S.TableCell>
+      </S.TableBody>)
   }
 
   return (
-  <
-    div style={{
-      padding: '10px',
-      borderRadius: '8px',
-      background: theme.palette.background.secondary,
-    }}
-  >
-    <S.TableBody>
+    <
+      div style={{
+        padding: '10px',
+        borderRadius: '8px',
+        background: theme.palette.background.secondary,
+      }}
+    >
+      <S.TableBody>
 
-      <S.TableCell
-        sx={{ gap: '5px' }}
-        style={{ width: '50%' }}
-      >
-        <div>{chain}</div>
-        <div style={{fontSize:'0.8em'}}>{midTitle}</div>
-        <div style={{fontSize:'0.8em', }}>{title}</div>
-        <div className={styles.muted} style={{fontSize: '0.8em'}}>{typeTX}</div>
-      </S.TableCell>
+        <S.TableCell
+          sx={{ gap: '5px' }}
+          style={{ width: '50%' }}
+        >
+          <Typography variant="h3">{chain}</Typography>
+          <Typography variant="body1">{midTitle}</Typography>
+          <Typography variant="body1">{title}</Typography>
+          <Typography variant="body2" className={styles.muted}>{typeTX}</Typography>
+          {!!detail &&
+            <Typography
+              variant="body2"
+              sx={{
+                cursor: 'pointer',
+              }}
+              onClick={() => {
+                setDropDownBox(!dropDownBox)
+                setDropDownBoxInit(false)
+              }}
+            >
+              More Information
+            </Typography>
+          }
+        </S.TableCell>
 
-      <S.TableCell
-        sx={{ gap: '5px' }}
-        style={{ width: '20%' }}
-      >
-        <div className={styles.muted}>{blockNumber}</div>
-      </S.TableCell>
+        <S.TableCell
+          sx={{ gap: '5px' }}
+          style={{ width: '20%' }}
+        >
+          <div className={styles.muted}>{blockNumber}</div>
+        </S.TableCell>
 
-      <S.TableCell sx={{ gap: "5px" }}>
-        {link &&
-          <a
-            href={link}
-            target={'_blank'}
-            rel='noopener noreferrer'
-            style={{color: theme.palette.mode === 'light' ? 'black' : 'white'}}
-          >
-            Blockexplorer
-          </a>
-        }
-        {!!detail &&
-          <S.TableCell
-            sx={{
-              gap: '5px',
-              cursor: 'pointer',
-            }}
-            onClick={() => {
-              setDropDownBox(!dropDownBox)
-              setDropDownBoxInit(false)
-            }}
-          >
-            More Information
-          </S.TableCell>
-        }
-        {button &&
-          <ButtonMUI
-            variant="contained"
-            color="primary"
-            sx={{
-              boder: '1.4px solid #506DFA',
-              borderRadius: '8px',
-              width: '180px'
-            }}
-            onClick={button.onClick}
-          >
-            {button.text}
-          </ButtonMUI>
-        }
+        <S.TableCell sx={{ gap: "5px" }}>
+          {link &&
+            <a
+              href={link}
+              target={'_blank'}
+              rel='noopener noreferrer'
+              style={{ color: theme.palette.mode === 'light' ? 'black' : 'white' }}
+            >
+              Blockexplorer
+            </a>
+          }
 
-      </S.TableCell>
-    </S.TableBody>
-    {renderDetailRedesign()}
-  </div>)
+          {button &&
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                boder: '1.4px solid #506DFA',
+                borderRadius: '8px',
+                width: '180px'
+              }}
+              onClick={button.onClick}
+            >
+              {button.text}
+            </Button>
+          }
+
+        </S.TableCell>
+      </S.TableBody>
+      {renderDetailRedesign()}
+    </div>)
 
 }
 
