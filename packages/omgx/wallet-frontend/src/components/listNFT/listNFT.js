23d87ac4e39b2c64b6463fb42d61071e34ab9d5c
sahil kashetwar <sahil@enya.ai>
2021-09-09 01:50:10 +0530

    added the logic to handle the url

--- packages/omgx/wallet-frontend/src/components/listNFT/listNFT.js
+++ packages/omgx/wallet-frontend/src/components/listNFT/listNFT.js
@@ -1,8 +1,25 @@
+/* eslint-disable quotes */
+/*
+Copyright 2019-present OmiseGO Pte Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 import React from 'react'
+import truncate from 'truncate-middle'
 import { connect } from 'react-redux'
 import { isEqual } from 'lodash'
 import * as styles from './listNFT.module.scss'
-
+import Copy from 'components/copy/Copy'
 class listNFT extends React.Component {
 
   constructor(props) {
@@ -93,10 +110,10 @@ class listNFT extends React.Component {
           <div className={styles.Table2}>
             <div className={styles.BasicText}><strong>{name}</strong> ({symbol})</div>
             {(attributes || []).map((attr, index)=>{
-              return <div className={styles.BasicTextLight} key={index}>{attr.trait_type}: {attr.value}</div>
+              return <div className={styles.BasicTextLight} key={index}>{attr.trait_type}: &npsp; <strong> {attr.value} </strong></div>
             })}
             <div className={styles.BasicTextLight}>UUID: {UUID}</div>
-            <div className={styles.BasicTextLight}>Address: {address}</div>
+            <div className={styles.BasicTextLight}>Address: {truncate(address, 8, 6, '...')} <Copy value={address} light={true} /></div>
             <div className={styles.BasicTextLight}>URI: {URL}</div>
             <div className={styles.BasicTextLight}>Time minted: {time}</div>
           </div>
