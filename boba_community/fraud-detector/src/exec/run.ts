60cbd283c905873f5e7c10e9ebcb3967957ffb3f
CAPtheorem <79423264+CAPtheorem@users.noreply.github.com>
2021-09-28 17:42:41 -0700

    Update run.ts

--- boba_community/fraud-detector/src/exec/run.ts
+++ boba_community/fraud-detector/src/exec/run.ts
@@ -55,8 +55,8 @@ const main = async () => {
     throw new Error('Must pass VERIFIER_WEB3_URL')
   }
 
-  const l2Provider = new providers.JsonRpcProvider(VERIFIER_WEB3_URL)  
-  const l1Provider  = new providers.JsonRpcProvider(L1_NODE_WEB3_URL)
+  const l2Provider = new providers.StaticJsonRpcProvider(VERIFIER_WEB3_URL)  
+  const l1Provider  = new providers.StaticJsonRpcProvider(L1_NODE_WEB3_URL)
 
   const service = new FraudProverService({
     l1RpcProvider: l1Provider,
